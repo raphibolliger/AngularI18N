600d6c87b4f7cf784d11f0defe59094d7dd5de49
Raphael Bolliger <raphibolliger@me.com>
2018-04-03 13:04:58 +0200

    added new elements for translation removed small elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
   title: string = 'AngularLocalization';
   now: Date = new Date();
   minutes: number = 0;
   gender: string = "o";
+  userName: string = "Raphael Bolliger";
+
+  public showWarning(): void {
+    alert("This warning text is always english, there is no possibility to achive a translation in component code.")
+  }
+
 }
